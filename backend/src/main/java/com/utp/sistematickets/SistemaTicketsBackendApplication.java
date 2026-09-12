a887b6184d77378ed0d519b277defdafcb9482fa
brayan <bryancris03@gmail.com>
2026-09-11 22:26:03 -0500

    chore: inicializar estructura base de spring boot

---
+++ backend/src/main/java/com/utp/sistematickets/SistemaTicketsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.utp.sistematickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTicketsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTicketsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/utp/sistematickets/SistemaTicketsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.sistematickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTicketsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
