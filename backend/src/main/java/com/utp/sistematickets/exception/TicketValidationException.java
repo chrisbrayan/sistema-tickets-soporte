4b9a2c9418c56a21de7e280045042b9a5dc8a56f
brayan <bryancris03@gmail.com>
2026-09-13 20:22:50 -0500

    feat: agregar excepcion ticketvalidation

---
+++ backend/src/main/java/com/utp/sistematickets/exception/TicketValidationException.java
@@ -0,0 +1,9 @@
+package com.utp.sistematickets.exception;
+
+public class TicketValidationException extends RuntimeException {
+
+    public TicketValidationException(String message) {
+        super(message);
+    }
+}
+
